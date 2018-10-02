a42c0c3829813b2ee350cb99b81024cea60304d0
Bishal Gautam <bishalkrgautam@gmail.com>
2018-10-01 19:08:53 -0500

    Attibute Based Routing

--- WebApiAttributeeRouting/App_Start/WebApiConfig.cs
+++ WebApiAttributeeRouting/App_Start/WebApiConfig.cs
@@ -14,6 +14,7 @@ namespace WebApiAttributeeRouting
             // Web API routes
             config.MapHttpAttributeRoutes();
 
+
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
