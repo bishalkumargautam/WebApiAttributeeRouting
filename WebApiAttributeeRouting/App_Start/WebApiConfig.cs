6d272ea5ca5583673fbd33c7375c3bacb9b25005
Bishal Gautam <bishalkrgautam@gmail.com>
2018-10-02 09:03:11 -0500

    Versioning using QueryString(But it has error, controllerName throws null exception)

--- WebApiAttributeeRouting/App_Start/WebApiConfig.cs
+++ WebApiAttributeeRouting/App_Start/WebApiConfig.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Web.Http.Dispatcher;
+using WebApiAttributeeRouting.Custom;
 
 namespace WebApiAttributeeRouting
 {
@@ -10,27 +12,38 @@ namespace WebApiAttributeeRouting
         public static void Register(HttpConfiguration config) 
         {
             // Web API configuration and services
+            
 
             // Web API routes
             config.MapHttpAttributeRoutes();
 
-            config.Routes.MapHttpRoute(
-               name: "Version1",
-               routeTemplate: "api/v1/{studentsversion1}/{id}",
-               defaults: new { id = RouteParameter.Optional,controller="StudentVersion1" }
-           );
+            // config.Routes.MapHttpRoute(
+            //    name: "Version1",
+            //    routeTemplate: "api/v1/{studentsversion1}/{id}",
+            //    defaults: new { id = RouteParameter.Optional,controller="StudentVersion1" }
+            //);
 
-            config.Routes.MapHttpRoute(
-               name: "Version2",
-               routeTemplate: "api/v2/{studentsversion2}/{id}",
-               defaults: new { id = RouteParameter.Optional,controller="StudentVersion2" }
-           );
+            // config.Routes.MapHttpRoute(
+            //    name: "Version2",
+            //    routeTemplate: "api/v2/{studentsversion2}/{id}",
+            //    defaults: new { id = RouteParameter.Optional,controller="StudentVersion2" }
+            //);
 
+            // config.Routes.MapHttpRoute(
+            //     name: "DefaultApi",
+            //     routeTemplate: "api/{controller}/{id}",
+            //     defaults: new { id = RouteParameter.Optional }
+            // );
+
+            //for querystring versioning
             config.Routes.MapHttpRoute(
-                name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
-                defaults: new { id = RouteParameter.Optional }
-            );
+               name: "DefaultRoute",
+               routeTemplate: "api/{controller}/{id}",
+               defaults: new { id = RouteParameter.Optional }
+           );
+            config.Services.Replace(typeof(IHttpControllerSelector), new CustomControllerSelector(config));
+           
+
         }
     }
 }
